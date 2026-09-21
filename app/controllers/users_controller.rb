a9e6f54454cf4d64d356fa9b0a33a5351f267c4a
unkuwun_x3 <joaogabrielrodrigues95@gmail.com>
2026-09-21 17:54:12 -0300

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      redirect_to @user, notice: 'Cadastro criado com sucesso!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(
+      :full_name,
+      :email,
+      :location,
+      :password,
+      :password_confirmation,
+      :bio
+    )
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+
+  has_secure_password
+  
+  validates :full_name, :email, :location, presence: true
+  validates :bio, length: { minimum: 30 }, allow_blank: false
+  validates :email, format: { with: EMAIL_REGEXP }, uniqueness: true
+end
