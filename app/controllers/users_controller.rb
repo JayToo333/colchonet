c6ba81ff2f9ba9b060220ce28d8874cd88910fd3
unkuwun_x3 <joaogabrielrodrigues95@gmail.com>
2026-09-23 15:15:02 -0300

    3.0

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    
+
     if @user.save
-      redirect_to @user, notice: 'Cadastro criado com sucesso!'
+      redirect_to @user, notice: "Cadastro criado com sucesso!"
     else
       render :new, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  
+
   resources :users
   root "home#index"
 end
