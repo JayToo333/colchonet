e0feb9ab2ad7c5d56bd8fb4c96e7774d37831c9f
unkuwun_x3 <joaogabrielrodrigues95@gmail.com>
2026-09-25 18:39:39 -0300

    a little reavaluation of until the capter 6 from the rails book

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,67 +1,19 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new(user_params)
+    @user = User.new
   end
 
   def create
-    @user = User.new(user_params)
-
-    if @user.save
-      #redirect_to @user, notice: "Cadastro criado com sucesso!"
-      return "User created " "#{@user[:full_name]}" "with id " "#{@user[:id]}"
-    else
-      #render :new, status: :unprocessable_entity
-      return "Error creating user " "#{@user[:full_name]}" "with id " "#{@user[:id]}"
-    end
+     @user = User.new(params.require(:user).permit!)
+     @user.save
+     redirect_to @user
   end
 
   def show
-    @user = User.find(@user[:id])
+    @user = User.find(params[:id])
   end
 
   def edit
-    @user = User.find(@user[:id])
-  end
-  
-  def update
-    @user = User.find(@user[:id])
-    if @user.update(updates)
-      return "User updated " "#{@user[:full_name]}" "with id " "#{@user[:id]}" "updated to " "#{updates}"
-    else
-      return "Error updating user: " "#{@user[:full_name]}" "with id " "#{@user[:id]}" "updated to " "#{updates}"
-    end
-  end
-
-  def user_params
-   {
-    full_name: "a",
-    email: "a10@gmail.com",
-    location: "a",
-    password: "a",
-    password_confirmation: "a",
-    bio: "1234567890123456789012345678901234567890"
-  }
-  end
-  
-  def updates
-   {
-    full_name: "b",
-    email: "b10@gmail.com",
-    location: "b",
-    password: "b",
-    password_confirmation: "b",
-  }
+    @user = User.find(params[:id])
   end
 end
-
-# private
-#  def user_params
-#     params.require(:user).permit(
-#       :full_name,
-#       :location,
-#       :password,
-#       :password_confirmation,
-#       :bio
-#       )
-#   end
-# end
